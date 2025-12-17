1932c7b88ed1799c7976a1ce02d74131758dded9
bala-amypo <bala@amypo.in>
2025-12-17 05:15:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,18 +1,5 @@
 package com.example.aiml.entity;
-
-import java.time.LocalDate;
-
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-@Entity
-//@Table(name="StudentTable")
 public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
@@ -22,11 +9,6 @@ public class Student {
         this.name = name;
         this.email = email;
     }
-    //to access the private(get-setter method)
-    // public  void setId(int id){
-    //     this.id=id;
-    // }
-    // public int getId(){
     public int getId() {
         return id;
     }
@@ -45,7 +27,8 @@ public class Student {
     public void setEmail(String email) {
         this.email = email;
     }
-    
+    public Student(){
         
-    // }
+    }
+    
 }
