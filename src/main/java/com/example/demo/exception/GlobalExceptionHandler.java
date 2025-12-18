5b1c6e71d6a61dc3254a1bf9da80a5e4acd8df3a
bala-amypo <bala@amypo.in>
2025-12-18 09:36:00 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,23 +1,32 @@
 package com.example.demo.exception;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.ResponseEntity;
+
 import java.util.Map;
 import java.util.HashMap;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler{
-@ExceptionHandler(ResourceNotFoundException.class) //runtime exception
-    public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){
-        return new ResponseEntity<>.(ex.getMessage(),HttpStatus.NOT_FOUND); 
 
-    }
+    @ExceptionHandler(ResourceNotFoundException.class) //runtime expection
+        public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex){
+            return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException)
-    Map<String,String> error=new HashMap<>();
-    ex.getBindingResult().getFieldErros().forEach(error -> errors.put(error.getField(),error.getDefaultMessage())
-    );
-    return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String,String>> handleMethod(MethodArgumentNotValidException mex){
+        
+        Map<String,String> errors=new HashMap<>();
+
+        mex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
